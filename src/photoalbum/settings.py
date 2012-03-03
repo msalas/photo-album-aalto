5a7fcbc2649e99e5cd886c7ee783064f4ca6827e
evarionovoa <evarionovoa@gmail.com>
2012-03-03 14:22:19 +0000

    (Hopefully) improved models

--- src/photoalbum/settings.py
+++ src/photoalbum/settings.py
@@ -106,7 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "C:/Users/EmB/workspace/photo-album-aalto/trunk/src/photoalbum/Templates"
+    "/Users/Evuski/Programming/workspace/photoalbum/src/photoalbum/Templates"
 )
 
 INSTALLED_APPS = (
