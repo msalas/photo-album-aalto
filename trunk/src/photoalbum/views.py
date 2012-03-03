d97f5380ae84c3cb152959cb015fa0a51ce1aa9f
evarionovoa@gmail.com <evarionovoa@gmail.com@8cdfbe50-a182-e90c-16aa-40f13621f172>
2012-03-03 18:30:49 +0000

    Added authentication, login, forms, signup and siginin

--- trunk/src/photoalbum/views.py
+++ trunk/src/photoalbum/views.py
@@ -1,43 +1,34 @@
-#from django.template.loader import get_template
-#from django.template import Context
+from django.template.loader import get_template
+from django.template import Context
 from django.template import RequestContext, loader
 from django.core.context_processors import csrf
-#from django.shortcuts import get_object_or_404
-#from django.contrib.auth.models import User
-from django.http import HttpResponse
-#from django.http import import HttpResponseRedirect
-#from photoalbum.models import *
-from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField( max_length=16 )
-    password = forms.CharField( max_length=16, widget=forms.PasswordInput )
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
+from photoalbum.models import *
+#from django import forms
+from photoalbum.forms import *
 
 
+##
+# Render the home page. 
 def main(request):
     t = loader.get_template('main.html')
- 
-   
-    # load the categories and the products ordered by rating
-#  categories = Category.objects.all()
-#  best_products = Product.objects.filter(stock_count__gt=0).order_by('-average_rating')[:10]
     
+    albums = Album.objects.filter(owner__id__exact=0)
     context = RequestContext(request, {
-  #      'categories'  : categories,
-  #      'products'    : best_products,
-    })
-    
-    # if the user is authenticated, then send info about the card in the request
-#if request.user.is_authenticated():
-#        albums = Album.objects.filter(owner=request.user.username)
-#covers = Image.objects.
-#        context.update({'albums': albums})
-    login_form = LoginForm()
-    context.update({'login_form': login_form})
-    
-    # if show-as-icons option, then send this option to the template
-    if request.GET.get('l') == 'icons':
-        context.update({'icons': 'OK'})
+    'user'  : request.user,
+    'album' : albums,
+    }) 
+    # if the user is authenticated
+    if request.user.is_authenticated():
+        user = request.user
+        template = loader.get_template('myalbums')
+        public_albums = Album.objects.filter(visible=True).filter(owner__id__exact=0)
+        context.update({'album': albums})
+    else:
+        login_form = LoginForm()
+        context.update({'login_form': login_form})
         
     # render the home page
     context.update(csrf(request))
