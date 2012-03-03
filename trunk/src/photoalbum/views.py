a6565d9f68e2848eccdb98e2e01fd7645269caab
ebuyukcan@gmail.com <ebuyukcan@gmail.com@8cdfbe50-a182-e90c-16aa-40f13621f172>
2012-03-03 19:17:40 +0000

    Album search added + database settings done

--- trunk/src/photoalbum/views.py
+++ trunk/src/photoalbum/views.py
@@ -9,23 +9,30 @@ from photoalbum.models import *
 #from django import forms
 from photoalbum.forms import *
 
-
+class LoginForm(forms.Form):
+    username = forms.CharField( max_length=16 )
+    password = forms.CharField( max_length=16, widget=forms.PasswordInput )
+	
 ##
 # Render the home page. 
 def main(request):
     t = loader.get_template('main.html')
-    
-    albums = Album.objects.filter(owner__id__exact=0)
+
     context = RequestContext(request, {
     'user'  : request.user,
-    'album' : albums,
     }) 
     # if the user is authenticated
     if request.user.is_authenticated():
         user = request.user
         template = loader.get_template('myalbums')
-        public_albums = Album.objects.filter(visible=True).filter(owner__id__exact=0)
-        context.update({'album': albums})
+        albums = Album.objects.filter(owner__id__exact=user.id)
+        public_albums = Album.objects.filter(visible=True).filter(owner__id__exact=user.id)
+        if len(albums) == 0:           
+            context.update({'public_albums': public_albums})
+        else:
+            context.update({'album': albums})
+            context.update({'public_albums': public_albums})
+            
     else:
         login_form = LoginForm()
         context.update({'login_form': login_form})
