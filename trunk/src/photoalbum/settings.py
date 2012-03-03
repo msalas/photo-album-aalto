a3036401c46a3852ce5dba5a5f4bdc59bd92c569
marc.salas@gmail.com <marc.salas@gmail.com@8cdfbe50-a182-e90c-16aa-40f13621f172>
2012-03-03 23:24:34 +0000

    NOTE!! Templates now is lower case --> templates

--- trunk/src/photoalbum/settings.py
+++ trunk/src/photoalbum/settings.py
@@ -106,7 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/Users/Evuski/Programming/workspace/photoalbum/src/photoalbum/Templates"
+    "/Users/Evuski/Programming/workspace/photoalbum/src/photoalbum/templates"
 )
 
 INSTALLED_APPS = (
